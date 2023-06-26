fc1b305a65aa8c63571be5dc344ade1ff4dcf509
Charlie <charlie@devoteam.com>
2023-06-26 18:43:51 +0200

    commit from home with extra files+

---
+++ C Piscine C 05/ex02/ft_recursive_power.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_is_negative.c                                   :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: cvan-roo <marvin@codam.nl>                   +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/06/08 12:26:39 by cvan-roo      #+#    #+#                 */
+/*   Updated: 2023/06/12 12:22:48 by cvan-roo      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+int ft_recursive_power(int nb, int power)
+{
+    if(power > 1)
+    {
+        power--;
+        nb *= ft_recursive_power(nb, power);
+    }
+    return nb;
+}
+
+int main()
+{
+    int res = ft_recursive_power(5, 5);
+    printf("\n %d \n \n", res);
+    return 0;
+}
