27cfce083a3b29fb7b833cb01785239a0f967c21
Charlie <charlie@devoteam.com>
2023-06-26 18:42:03 +0200

    commit from hom

--- C Piscine C 05/ex01/ft_recursive_factorial.c
+++ C Piscine C 05/ex01/ft_recursive_factorial.c
@@ -1,42 +1,35 @@
-    /* ************************************************************************** */
-    /*                                                                            */
-    /*                                                        ::::::::            */
-    /*   ft_is_negative.c                                   :+:    :+:            */
-    /*                                                     +:+                    */
-    /*   By: cvan-roo <marvin@codam.nl>                   +#+                     */
-    /*                                                   +#+                      */
-    /*   Created: 2023/06/08 12:26:39 by cvan-roo      #+#    #+#                 */
-    /*   Updated: 2023/06/12 12:22:48 by cvan-roo      ########   odam.nl         */
-    /*                                                                            */
-    /* ************************************************************************** */
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_is_negative.c                                   :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: cvan-roo <marvin@codam.nl>                   +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/06/08 12:26:39 by cvan-roo      #+#    #+#                 */
+/*   Updated: 2023/06/12 12:22:48 by cvan-roo      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
 
-    #include <unistd.h>
-    #include <stdio.h>
+#include <unistd.h>
+#include <stdio.h>
 
-    int ft_recursive_factorial (int nb)
-    {
-        int res;
+int ft_recursive_factorial(int nb)
+{
+    int res = 1;
 
-        
-        if(0 < nb)
-        {
-            res = nb;
-            nb--;
-            res *= nb;
-            res += ft_recursive_factorial(nb);
-        }
-        else
-        {
-            return 1;
-        }
+    if (nb > 1)
+    {    
+        res = nb;
+        nb--;
+        res *= ft_recursive_factorial(nb);
+    }  
+    return res;
+}
 
-        return res;
-    }
-
-    int main()
-    {
-        int example = 7;
-        int res = ft_recursive_factorial(example);
-        printf("%d", res);
-        return 0;
-    }
+int main()
+{
+    int example = 7;
+    int res = ft_recursive_factorial(example);
+    printf("%d", res);
+    return 0;
+}
